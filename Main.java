58decc1799806e5cd3e25067aa61f3b668c9bb78
akulaanilkumar54 <anilsathyam1234@gmail.com>
2023-03-27 12:15:11 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+package FrameworkSpring;
+//This is first file presented in it
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Spring framework");
+
+    }
+}
